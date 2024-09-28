1868e68e724083caef1b5cd760fbcf92747f419b
Pranshu Thakur <63305411+Pranshu-Thakur@users.noreply.github.com>
2024-09-29 01:18:45 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,27 @@
+#conditional operators 
+x=1
+y=2
+z=3
+
+res1 = x==y
+res2 = z > x
+res3 = z != y 
+
+res4 = res2 or res1
+res5 = res2 and res1
+
+res6 = res2 and  not res3
+
+
+print('res1' , res1)
+print('res2' ,res2)
+print('res3' ,res3)
+print('res4' ,res4)
+print('res5' ,res5)
+
+print('res6' ,res6)
+
+
+#There are and not or which are self explanatory.
+# and there is == , < , > , !=
+
